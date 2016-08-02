c56b1277b48267bac826669a759bcb05929875e2
Judith Eckle-Kohler <jek@eckle-kohler.de>
2016-08-02 10:30:17 +0200

    initial commit of code

---
+++ de.tudarmstadt.ukp.experiments.modality/src/main/java/corpus/VerbCountingPipelineEnglish.java
@@ -0,0 +1,97 @@
+/*******************************************************************************
+ * Copyright 2016
+ * Ubiquitous Knowledge Processing (UKP) Lab
+ * Technische Universität Darmstadt
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package corpus;
+
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+import static org.apache.uima.fit.factory.CollectionReaderFactory.createReaderDescription;
+
+import org.apache.uima.fit.pipeline.SimplePipeline;
+
+import de.tudarmstadt.ukp.dkpro.core.io.text.TextReader;
+import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordSegmenter;
+import de.tudarmstadt.ukp.dkpro.core.treetagger.TreeTaggerPosTagger;
+
+/**
+ * Simple pipeline using a reader and a writer from the DKPro Core component collection. 
+ * The VerbCounter counts occurrences of verb tokens (tagged VV.*)
+ *
+ * @author Judith Eckle-Kohler
+ *
+ */
+public class VerbCountingPipelineEnglish
+{
+
+	static String UBY_HOME = System.getenv("UBY_HOME");
+	final static String corpusDirectory = UBY_HOME +"/reuters/";
+
+    public static void main(String[] args) throws Exception
+    {
+
+        SimplePipeline.runPipeline(
+                createReaderDescription(
+                		TextReader.class,
+                		TextReader.PARAM_SOURCE_LOCATION, UBY_HOME+"/reuters",
+                		TextReader.PARAM_PATTERNS, "*",
+                        TextReader.PARAM_LANGUAGE, "en"),
+                createEngineDescription(StanfordSegmenter.class),
+                createEngineDescription(TreeTaggerPosTagger.class),
+                createEngineDescription(VerbCounter.class,
+                		VerbCounter.PARAM_OUTPUT_FILE,
+                		UBY_HOME+"/verbClasses/" +"reuters" +"VerblemmaList")
+                );
+
+
+    	/*
+        		SimplePipeline.runPipeline(
+        			createReaderDescription(
+	                 		ImsCwbReader.class,
+							ImsCwbReader.PARAM_ENCODING, "UTF-8",
+							// C:\Users\Judith\corpora\dewac\sdewac
+			                ResourceCollectionReaderBase.PARAM_SOURCE_LOCATION,     new File("C:/Users/Judith/corpora/dewac/filtered/").getAbsolutePath(),
+			                ResourceCollectionReaderBase.PARAM_PATTERNS, new String[] { "*.tagged" },
+			                ResourceCollectionReaderBase.PARAM_LANGUAGE, "de"),
+	                createEngineDescription(GermanSeparatedParticleAnnotator.class),
+	                createEngineDescription(VerbCounter.class,
+                		VerbCounter.PARAM_OUTPUT_FILE,
+                		UBY_HOME+"/verbClasses/" +"sdewac" +"VerblemmaList")
+                );
+
+*/
+
+ /*
+          			SimplePipeline.runPipeline(
+					createReaderDescription(
+							WikipediaArticleReader.class,
+							WikipediaReaderBase.PARAM_HOST, "localhost",
+							WikipediaReaderBase.PARAM_DB, "wikiapi_de_20090618",
+							WikipediaReaderBase.PARAM_USER, "eckle",
+							WikipediaReaderBase.PARAM_PASSWORD, "JupiterPluto",
+							WikipediaReaderBase.PARAM_LANGUAGE, Language.german
+			                ),
+                createEngineDescription(LanguageToolSegmenter.class),
+                createEngineDescription(TreeTaggerPosLemmaTT4J.class),
+                createEngineDescription(GermanSeparatedParticleAnnotator.class),
+                createEngineDescription(VerbCounter.class,
+                		VerbCounter.PARAM_OUTPUT_FILE,
+                		UBY_HOME+"/verbClasses/" +"wikipedia" +"VerblemmaList")
+					);
+*/
+
+
+    }
+}
